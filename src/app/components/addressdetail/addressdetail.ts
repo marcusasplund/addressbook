ae1d974ce0af31d71bbc8f6d772b9c95c3e67679
Marcus Asplund <rootfood@gmail.com>
2016-05-24 10:04:53 +0200

    Update addressdetail.ts

--- src/app/components/addressdetail/addressdetail.ts
+++ src/app/components/addressdetail/addressdetail.ts
@@ -23,28 +23,42 @@ function emailValidator(control) {
     CountryService
   ],
   template: `
-  <md-card>
-    <h4 md-line *ngIf="selectedAddress.id">Editing
-      {{originalName}}</h4>
-    <h4 md-line *ngIf="!selectedAddress.id">Create New Address</h4>
-    <md-card-content>
-      <form novalidate [ngFormModel]="order">
-        <md-input [ngFormControl]="order.controls['firstName']" [(ngModel)]="selectedAddress.firstName" placeholder="First name"></md-input>
-        <md-input [ngFormControl]="order.controls['lastName']" [(ngModel)]="selectedAddress.lastName" placeholder="Last name"></md-input>
-        <md-input [ngFormControl]="order.controls['email']" [(ngModel)]="selectedAddress.email" placeholder="Email"></md-input>
-        <label>Country</label>
-        <select [ngFormControl]="order.controls['country']" [(ngModel)]="selectedAddress.country">
-          <option *ngFor="let country of countries" value="{{country}}">{{country}}</option>
-        </select>
-      </form>
-    </md-card-content>
-    <md-card-actions>
-      <button md-raised-button type="submit" (click)="cancelled.emit(selectedAddress)">Cancel</button>
-      <button md-raised-button color="primary" [disabled]="!order.valid" type="submit" (click)="saved.emit(selectedAddress)">Save
-        <md-icon class="material-icons">save</md-icon>
-      </button>
-    </md-card-actions>
-  </md-card>
+    <md-card>
+      <h4 md-line *ngIf="selectedAddress.id">Editing
+        {{originalName}}</h4>
+      <h4 md-line *ngIf="!selectedAddress.id">Create New Address</h4>
+      <md-card-content>
+        <form novalidate [ngFormModel]="order">
+          <md-input
+            [ngFormControl]="order.controls['firstName']"
+            [(ngModel)]="selectedAddress.firstName"
+            placeholder="First name"></md-input>
+          <md-input
+            [ngFormControl]="order.controls['lastName']"
+            [(ngModel)]="selectedAddress.lastName"
+            placeholder="Last name"></md-input>
+          <md-input
+            [ngFormControl]="order.controls['email']"
+            [(ngModel)]="selectedAddress.email"
+            placeholder="Email"></md-input>
+          <label>Country</label>
+          <select [ngFormControl]="order.controls['country']" [(ngModel)]="selectedAddress.country">
+            <option *ngFor="let country of countries" value="{{country}}">{{country}}</option>
+          </select>
+        </form>
+      </md-card-content>
+      <md-card-actions>
+        <button md-raised-button type="submit" (click)="cancelled.emit(selectedAddress)">Cancel</button>
+        <button
+          md-raised-button
+          color="primary"
+          [disabled]="!order.valid"
+          type="submit"
+          (click)="saved.emit(selectedAddress)">Save
+          <md-icon class="material-icons">save</md-icon>
+        </button>
+      </md-card-actions>
+    </md-card>
   `
 })
 export class AddressDetail {
