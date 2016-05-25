e5d5b6f45989eff406906b6849a5135f89cc8cd5
Marcus Asplund <rootfood@gmail.com>
2016-05-25 10:26:52 +0200

    Corrected form submit

--- src/app/components/addressdetail/addressdetail.ts
+++ src/app/components/addressdetail/addressdetail.ts
@@ -1,16 +1,8 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {FORM_BINDINGS, FORM_DIRECTIVES, ControlGroup, FormBuilder, Validators} from '@angular/common';
+import {FORM_BINDINGS, FORM_DIRECTIVES, ControlGroup, FormBuilder, Validators, Control} from '@angular/common';
 import {CountryService} from '../../../services/country-service';
 import {AddressService, Address} from '../../../addresses';
 import {Observable} from 'rxjs/Observable';
-
-function emailValidator(control) {
-  const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-
-  if (!EMAIL_REGEXP.test(control.value)) {
-    return {invalidEmail: true};
-  }
-}
 
 @Component({
   selector: 'address-detail',
@@ -28,8 +20,8 @@ function emailValidator(control) {
   <h4 md-line *ngIf="selectedAddress.id">Editing
     {{originalName}}</h4>
   <h4 md-line *ngIf="!selectedAddress.id">Create New Address</h4>
-  <md-card-content>
-    <form novalidate [ngFormModel]="order">
+  <form novalidate (submit)="onSubmit($event)" [ngFormModel]="order">
+    <md-card-content>
       <md-input
         type="text"
         [ngFormControl]="order.controls['firstName']"
@@ -48,19 +40,14 @@ function emailValidator(control) {
       <select [ngFormControl]="order.controls['country']" [(ngModel)]="selectedAddress.country">
         <option *ngFor="let country of countries" [ngValue]="country">{{country}}</option>
       </select>
-    </form>
-  </md-card-content>
-  <md-card-actions>
-    <button md-raised-button type="submit" (click)="cancelled.emit(selectedAddress)">Cancel</button>
-    <button
-      md-raised-button
-      color="primary"
-      [disabled]="!order.valid"
-      type="submit"
-      (click)="saved.emit(selectedAddress)">Save
-      <md-icon class="material-icons">save</md-icon>
-    </button>
-  </md-card-actions>
+    </md-card-content>
+    <md-card-actions>
+      <button md-raised-button type="submit" (click)="cancelled.emit(selectedAddress)">Cancel</button>
+      <button md-raised-button color="primary" [disabled]="!order.valid" type="submit">Save
+        <md-icon class="material-icons">save</md-icon>
+      </button>
+    </md-card-actions>
+  </form>
 </md-card>
   `
 })
@@ -82,6 +69,18 @@ export class AddressDetail {
     });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    this.saved.emit(this.selectedAddress);
+  }
+
+  emailValidator(control: Control) {
+    const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    if (!EMAIL_REGEXP.test(control.value)) {
+      return {invalidEmail: true};
+    }
+  }
+
   ngOnInit() {
     if (!this.selectedAddress.country) {
       this.selectedAddress.country = 'Sweden';
@@ -89,9 +88,8 @@ export class AddressDetail {
     this.order = this.fb.group({
       firstName: [this.selectedAddress.firstName || '', Validators.required],
       lastName: [this.selectedAddress.lastName || '', Validators.required],
-      email: [this.selectedAddress.email || '', Validators.compose([emailValidator])],
+      email: [this.selectedAddress.email || '', Validators.compose([Validators.required, this.emailValidator])],
       country: [this.selectedAddress.country || '', Validators.required]
     });
   }
 }
-
