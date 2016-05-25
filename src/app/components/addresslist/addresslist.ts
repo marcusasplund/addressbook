72f7a6fdb38f6181fbb359f39a8d75490455a19a
Marcus Asplund <rootfood@gmail.com>
2016-05-25 23:47:23 +0200

    indents

--- src/app/components/addresslist/addresslist.ts
+++ src/app/components/addresslist/addresslist.ts
@@ -1,4 +1,9 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import {Address} from '../../../addresses';
 
 //-------------------------------------------------------------------
@@ -7,15 +12,19 @@ import {Address} from '../../../addresses';
 @Component({
   selector: 'address-list',
   template: `
-  <md-list>
+<md-list>
   <md-list-item *ngFor="let address of addresses">
     <h4 md-line>{{address.name}}</h4>
     <p md-line>
       <span>{{address.description}}</span>
       <span class="demo-secondary-text">{{address.city}}</span>
     </p>
-      <button md-mini-fab (click)="selected.emit(address)"><md-icon class="material-icons">edit</md-icon></button>
-      <button md-mini-fab (click)="deleted.emit(address); $event.stopPropagation();"><md-icon class="material-icons">close</md-icon></button>
+    <button md-mini-fab (click)="selected.emit(address)">
+      <md-icon class="material-icons">edit</md-icon>
+    </button>
+    <button md-mini-fab (click)="deleted.emit(address); $event.stopPropagation();">
+      <md-icon class="material-icons">close</md-icon>
+    </button>
   </md-list-item>
 </md-list>
   `
